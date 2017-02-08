0c927c119cfaeec1e6d8f46fb1a6c55699bfda79
Tim Bellefleur <nomoon@phoebus.ca>
2017-02-08 15:56:19 -0800

    If APIError gets a JSON response, it will parse and pretty-print it.

--- lib/lunanode/api_error.rb
+++ lib/lunanode/api_error.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 require "cgi"
+require "json"
 
 module Lunanode
   # A RuntimeError raised when the API call returns unsuccessfully.
   class APIError < RuntimeError
     def to_s
-      CGI.unescapeHTML(super)
+      unescaped = CGI.unescapeHTML(super)
+      return unescaped unless unescaped.start_with?("\"{")
+      JSON.pretty_generate(JSON.parse(unescaped.gsub(/\A"|"\z/, "")))
+    rescue JSON::ParserError
+      unescaped
     end
   end
 end
