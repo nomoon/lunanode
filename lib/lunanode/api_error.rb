59014700acec3e46a55e59f0acd23adbe782b36c
Tim Bellefleur <nomoon@phoebus.ca>
2017-02-08 14:14:00 -0800

    Unescape APIError messages.

--- lib/lunanode/api_error.rb
+++ lib/lunanode/api_error.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+require "cgi"
+
 module Lunanode
   # A RuntimeError raised when the API call returns unsuccessfully.
   class APIError < RuntimeError
+    def to_s
+      CGI.unescapeHTML(super)
+    end
   end
 end
